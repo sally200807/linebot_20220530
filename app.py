93ea268cbd96444584002287b5e6ecacd1d1146a
sally200807 <sally200807@gmail.com>
2022-06-02 15:40:51 +0800

    fix3

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi('5kQpaioC9HTymyysrMALpcb6IJ5nHTuRhFQPgSuohKDE7ab2TxxlDiV4ELBvZqMhLi5ET6xCQVX4wLI1ihPTxYPeLtGIGij++Eljyi3G8AdnnXCOWMMyibJzlC0K8HfeSFquGmtvVz9hMKoKMFzSmAdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('566551dc7e730127a19e0943e13045f5')
 
 
 @app.route("/callback", methods=['POST'])
@@ -37,9 +37,19 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    hi = "hi" or "測試你好" or "你好"
+    r = "那個……請問你在說什麼？"
+
+    if msg == hi:
+        r = "朋友你好，我是測試機器人"
+    elif msg == "你吃飽沒"
+        r = "我吃飽了!因為我有剛剛你的支持!"
+
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text= r))
 
 
 if __name__ == "__main__":
