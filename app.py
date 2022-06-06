438bd81f72f8fc40fc3366775e928df9d00e0349
sally200807 <sally200807@gmail.com>
2022-06-06 10:17:28 +0800

    fix3.3

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    hi = "hi" or "測試你好" or "你好"
+    hi = "hi"
     r = "那個……請問你在說什麼？"
 
     if msg == hi:
